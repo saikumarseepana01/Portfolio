3c0cd54d8d2609b5e6e3c37d86ebcf66dd7802e9
Sai kumar seepana <164468934+saikumarseepana01@users.noreply.github.com>
2025-02-05 00:03:39 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.getElementById('email-link').addEventListener('click', function() {
+    window.location.href = 'mailto:saikumarseepana01@gmail.com';
+});
+
+document.getElementById('phone-link').addEventListener('click', function() {
+    window.location.href = 'tel:+919705954462';
+});
+
+document.getElementById('linkedin-link').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/saikumar-seepana-a65264230', '_blank');
+});
